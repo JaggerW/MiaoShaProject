4d0af83e48b68e2a94012ce23f913804d77c7ccc
jaggerw <jaggerw12138@163.com>
2020-09-21 15:12:10 +0800

    HelloWorld

---
+++ src/main/java/com/nju/miaosha/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.nju.miaosha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: jaggerw
+ * @Description: DemoController
+ * @Date: 2020/9/21
+ */
+
+@Controller
+@RequestMapping(value = "/demo")
+public class DemoController {
+
+    @GetMapping("/test")
+    public String thymeleaf(Model model){
+        model.addAttribute("name","jaggerw");
+        return "hello";
+    }
+
+}
